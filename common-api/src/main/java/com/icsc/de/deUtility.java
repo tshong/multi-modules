a7d1c08ccc88ea165e77ebdb66fd59538a794eb2
saihong <tangsaihong@gmail.com>
2021-03-01 20:00:09 +0800

    Initial commit

---
+++ common-api/src/main/java/com/icsc/de/deUtility.java
@@ -0,0 +1,7 @@
+package com.icsc.de;
+
+public class deUtility {
+    public static void go() {
+        System.out.println("Go");
+    }
+}
---
+++ server/src/main/java/com/icsc/mq/server/MQServer.java
@@ -0,0 +1,9 @@
+package com.icsc.mq.server;
+
+import com.icsc.de.deUtility;
+
+public class MQServer {
+    public static void main(String[] args) {
+        deUtility.go() ;
+    }
+}
